69ef3b890f69fe625f8e5b01023dc6c29cfa33d2
Luketss <lucassilluzio@gmail.com>
2021-01-24 13:08:59 -0300

    define index view and selectall registers in enderecos.db

--- cadastro_endereco/website/views.py
+++ cadastro_endereco/website/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Endereco
 
 def index(request):
-    return HttpResponse("Hello, world. This is adress index")
+    adress_list = Endereco.objects.order_by('cep')
+    template = loader.get_template('website/index.html')
+    context = {
+        'adress_list': adress_list
+    }
+    return HttpResponse(template.render(context, request))
+
